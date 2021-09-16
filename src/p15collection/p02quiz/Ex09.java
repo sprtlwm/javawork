c8afedbcbf2975bb8d3387d621d4955c0822d83b
sprtlwm <89769744+sprtlwm@users.noreply.github.com>
2021-09-16 11:47:20 +0900

    commit

---
+++ src/p15collection/p02quiz/Ex09.java
@@ -0,0 +1,19 @@
+package p15collection.p02quiz;
+
+import java.util.List;
+
+public class Ex09 {
+	public void removeLastMax(List<Integer>list){
+		// list의 마지막 가장 큰값을 삭제
+		int index = 0;
+		int max = list.get(0);
+		
+		for(int i = 1; i<list.size(); i++ ) {
+			if(max <= list.get(i)) {
+				max = list.get(i);
+				index = i;
+			}
+		}
+			list.remove(index);
+	}
+}
