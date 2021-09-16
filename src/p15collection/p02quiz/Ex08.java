6e9442c9d47009c4a334bca39cb24527a4fbc79b
sprtlwm <89769744+sprtlwm@users.noreply.github.com>
2021-09-16 11:15:49 +0900

    commit

---
+++ src/p15collection/p02quiz/Ex08.java
@@ -0,0 +1,20 @@
+package p15collection.p02quiz;
+
+import java.util.List;
+
+public class Ex08 {
+	public int maxLastIndex(List<Integer>list) {
+		//list의 최대값이 있는 마지막 인덱스
+		int lastindex = 0;
+		int max = list.get(0);
+		
+		for(int i = 1; i<list.size(); i++) {
+			if(list.get(i) >= max) {
+				max = list.get(i);
+				lastindex = i;
+			}
+		}
+		
+		return lastindex;
+	}
+}
